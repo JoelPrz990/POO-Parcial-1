223a3e50db1c914af2a67c32e92ee1af92da6cbb
Joel Perez Ortiz <jopeor.2001@outlook.com>
2021-03-02 08:20:27 -0600

    No se programar

---
+++ src/parcial1/Celular.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial1;
+
+/**
+ *
+ * @author USER
+ */
+   
+public class Celular {
+    
+    private String marca;
+    private String capacidad;
+    private String color;
+    private String tamaño;
+    private String modelo;
+    private double precio;
+    private boolean chino;
+    private long numero;
+    
+//Private String
+    //métodos
+    //método especial, constructor
+//metodos o funciones
+    //sintaxis de creación de métodos
+    //ModAcces tipoRetorno Nombre(tipoDato nombreParam)
+    //void => vacío 
+    //parametros => variables olcales 
+ 
+    
+    public void llamar(int numero, int lada){}
+ //(int numero) variable local
+ //contexto reducido
+ //algo
+ 
+    
+//contexto
+    //Alcance que puede tener una variable
+    
+//Modificador de acceso
+
+//atributos
+    //marca String, capacidad String, color String, costo double, modelo string, 
+           
+    public void llamar(int numero){
+        System.out.println("Marcando al numero: "+numero +"...");
+    }
+    public String ObtenerMarca(){
+    return marca;
+    }
+    public void EstablecerMarca(String marca){
+    
+    }
+}
---
+++ src/parcial1/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+/**
+ *
+ * @author Joel
+ */
+public class Main {
+    //psvm
+    public static void main(String[] args){
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    Scanner sc= new Scanner(System.in);
+    int[] agenda; //falta inicializar owo
+    int tam;//variable sin iniciar
+    System.out.println("De que tamano quierez la agenda?: ");
+    tam= sc.nextInt();//inicializar variable
+    agenda = new int[tam];//inicialización
+    
+    }
+} 
+
