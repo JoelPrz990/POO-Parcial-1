f107a07a3c93cbf51646c46a0558478e98edbc6d
Joel Perez Ortiz <jopeor.2001@outlook.com>
2021-03-07 11:12:35 -0600

    all right

---
+++ src/parcial1/Parcial1P1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial1;
+import java.util.Scanner;
+/**
+ *
+ * @author USER
+ */
+public class Parcial1P1 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cuantas lineas quieres que haya?");
+        n = sc.nextInt();
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
