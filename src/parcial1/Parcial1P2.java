3f5a9f7305ebb1e8ec85c0bd065c2443e2b4e7e2
Joel Perez Ortiz <jopeor.2001@outlook.com>
2021-03-07 14:37:01 -0600

    todo oK

--- src/parcial1/Parcial1P2.java
+++ src/parcial1/Parcial1P2.java
@@ -16,12 +16,15 @@ public class Parcial1P2{
         System.out.println("¿Cuantas lineas quieres que haya?");
         n= sc.nextInt();
         
-        for(int i=0; i<n; i++){
-            for(int j=(n-1); j<i; j--){
+         for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=(i*2)-1; k++){
                 System.out.print("*");
             }
-            System.out.println("");
-        }
+             System.out.println("");
+         }
     }
-    
 }
+    
