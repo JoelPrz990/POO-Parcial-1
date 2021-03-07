fdc70181c9225e43914fe0c52cec517df31b9fc8
Joel Perez Ortiz <jopeor.2001@outlook.com>
2021-03-07 12:16:35 -0600

    falta el 2

--- src/parcial1/Parcial1P2.java
+++ src/parcial1/Parcial1P2.java
@@ -4,18 +4,20 @@
  * and open the template in the editor.
  */
 package parcial1;
-
+import java.util.Scanner;
 /**
  *
  * @author USER
  */
-public class segundo {
-      private int noLinea;
-      
+public class Parcial1P2{
     public static void main(String[] args) {
+        int n;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("¿Cuantas lineas quieres que haya?");
+        n= sc.nextInt();
         
-        for(int i=0; i<5; i++){
-            for(int j=0; j<= i; j++){
+        for(int i=0; i<n; i++){
+            for(int j=(n-1); j<i; j--){
                 System.out.print("*");
             }
             System.out.println("");
---
+++ src/parcial1/Parcial1P3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial1;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ *
+ * @author USER
+ */
+public class Parcial1P3 {
+    public static void main(String[] args) {
+        int n;
+        int[] ordenar;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("¿Cuantos numeros quieres agregar? ");
+        n= sc.nextInt();
+        ordenar = new int[n]; 
+        for(int i=0; i<n; i++){
+            System.out.print("Ingresa el numero en la posición: ["+(i+1)+"] ");
+            ordenar[i] = sc.nextInt();
+        }
+        Arrays.sort(ordenar);
+        System.out.print("Los numeros en orden son: ");
+        for(int i=0; i<ordenar.length; i++ ){
+            System.out.print(" "+ordenar[i]+",");
+        }
+    }
+}
